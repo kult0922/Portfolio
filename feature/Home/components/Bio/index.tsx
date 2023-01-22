0398f9540698f862e0a556c203d7a925b0ba26e5
kult0922 <38486600+kult0922@users.noreply.github.com>
2023-01-22 12:48:16 +0900

    add month

--- feature/Home/components/Bio/index.tsx
+++ feature/Home/components/Bio/index.tsx
@@ -7,14 +7,14 @@ const Bio = () => {
       <Section title="Bio"></Section>
       <div className="mt-4">
         <Experience
-          year="2019"
+          year="2019.3"
           title="千葉大学 情報画像学科 学士課程 卒業"
         ></Experience>
         <Experience
-          year="2021"
+          year="2021.3"
           title="千葉大学融合理工学府 情報科学専攻 修士課程 卒業"
         ></Experience>
-        <Experience year="2021" title="楽天株式会社 入社"></Experience>
+        <Experience year="2021.4" title="楽天株式会社 入社"></Experience>
       </div>
     </div>
   )
