a8b94c87c0ec933dd214c1765736cd78c8ac8331
kult0922 <38486600+kult0922@users.noreply.github.com>
2023-01-22 12:41:46 +0900

    add Bachelor

--- feature/Home/components/Bio/index.tsx
+++ feature/Home/components/Bio/index.tsx
@@ -7,6 +7,10 @@ const Bio = () => {
       <Section title="Bio"></Section>
       <div className="mt-4">
         <Experience
+          year="2019"
+          title="千葉大学 情報画像学科 学士課程 卒業"
+        ></Experience>
+        <Experience
           year="2021"
           title="千葉大学融合理工学府 情報科学専攻 修士課程 卒業"
         ></Experience>
