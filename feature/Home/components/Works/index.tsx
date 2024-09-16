ff91a3641c0e587a9a3bc0a8c281c0b41b29e9c3
kurisaki_kazuma <38486600+kult0922@users.noreply.github.com>
2024-09-16 22:44:36 +0900

    fix works code links

--- feature/Home/components/Works/index.tsx
+++ feature/Home/components/Works/index.tsx
@@ -9,14 +9,14 @@ const Works = () => {
         <Work
           imagePath="/image/minecraft_dot.png"
           webUrl="https://www.minecraft-dot.pictures"
-          codeUrl="https://github.com/kult0922/kurisakien"
+          codeUrl="https://github.com/kult0922/minecraftDot"
           title="Minecraft Dot"
           description="写真をマインクラフトのドット絵に変換・編集できるアプリ"
         />
         <Work
           imagePath="/image/kurisakien.png"
           webUrl="https://kurisaki-en.com/"
-          codeUrl="https://github.com/kult0922/minecraftDot"
+          codeUrl="https://github.com/kult0922/kurisakien"
           title="「栗崎園」ECサイト"
           description="お茶農家「栗崎園」ECサイト"
         />
